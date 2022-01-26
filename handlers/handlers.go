8df4e86de77adce4238e40a31833515bc8c5334d
serhatkarakoca <serhatkarakoca@gmail.com>
2022-01-26 12:44:54 +0300

    register email ve password validation yapıldı

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -25,6 +25,25 @@ func Handlers() {
 			return err
 		}
 
+		if helpers.IsEmpty(registerModel.Email) || helpers.IsEmpty(registerModel.Password) {
+			return c.Status(400).JSON(&fiber.Map{
+				"success": false,
+				"message": "You must fill in all fields",
+			})
+		}
+
+		if !helpers.IsEmail(registerModel.Email) {
+			return c.Status(400).JSON(&fiber.Map{
+				"success": false,
+				"message": "Please enter valid email",
+			})
+		}
+
+		var passwordValidation = helpers.IsPasswordStrong(registerModel.Password)
+		if !passwordValidation.Success {
+			return c.Status(400).JSON(passwordValidation)
+		}
+
 		row, err := db.DB().Query("Select * from user where email = ?", registerModel.Email)
 		if err != nil {
 			log.Fatal(err)
--- helpers/stringHelpers.go
+++ helpers/stringHelpers.go
@@ -1,8 +1,68 @@
 package helpers
 
+import (
+	"net/mail"
+	"unicode"
+)
+
+type FieldError struct {
+	Message string
+	Success bool
+}
+
 func IsEmpty(data string) bool {
 	if len(data) == 0 {
 		return true
 	}
 	return false
+}
+
+func IsEmail(data string) bool {
+	_, err := mail.ParseAddress(data)
+	return err == nil
+
+}
+
+func IsPasswordStrong(pass string) FieldError {
+	var letters = 0
+	var number, upper, special bool
+	for _, c := range pass {
+		switch {
+		case unicode.IsNumber(c):
+			number = true
+		case unicode.IsUpper(c):
+			upper = true
+			letters++
+		case unicode.IsSymbol(c) || unicode.IsPunct(c):
+			special = true
+		case unicode.IsLetter(c) || c == ' ':
+			letters++
+		default:
+		}
+	}
+	if letters < 7 {
+		return FieldError{
+			Message: "Password must be at least 7 characters",
+			Success: false,
+		}
+	} else if !number {
+		return FieldError{
+			Message: "Password must have at least 1 number",
+			Success: false,
+		}
+	} else if !upper {
+		return FieldError{
+			Message: "Password must have at least 1 uppercase",
+			Success: false,
+		}
+	} else if !special {
+		return FieldError{
+			Message: "Password must have special character",
+			Success: false,
+		}
+	} else {
+		return FieldError{
+			Success: true,
+		}
+	}
 }
